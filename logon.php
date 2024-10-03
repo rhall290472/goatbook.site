a47dd1f9d2d74938b328536965c15d982240acbb
rhall290472 <56454660+rhall290472@users.noreply.github.com>
2024-10-02 18:49:01 -0600

    Cleanup

--- logon.php
+++ logon.php
@@ -155,7 +155,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
       </form>
     </div>
-  </center> -->
+  </center>
 
   <?php include('Footer.php'); ?>
 </body>
