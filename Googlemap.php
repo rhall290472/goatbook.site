9e4bfc2ac7a9a96d8de6ae3c8ca65d80d192349d
Richard Hall <rhall290472@gmail.com>
2024-11-12 10:20:23 -0700

    Added google map to index page

--- Googlemap.php
+++ Googlemap.php
@@ -5,6 +5,8 @@ if (!session_id()) {
 include('cGOAT.php');
 $cGOAT = cGOAT::getInstance();
 
+ini_set("memory_limit", "90000M");
+
 /*
 !==============================================================================!
 !\                                                                            /!
@@ -43,55 +45,68 @@ $cGOAT = cGOAT::getInstance();
 
   $csv_hdr = "WKT, Name, Area, Activity 1, Activity 2, Site Link";
   $csv_output = "";
-?>
-  <table class='table'  style='width:1024';>
-  <tr>
-  <th>WKT</th>
-  <th>Name</th>
-  <th>Area</th>
-  <th>Activity 1</th>
-  <th>Activity 2</th>
-  <th>Site Link</th>
-  </tr>
-  <?php
+  ?>
+  <table class='table' style='width:1024' ;>
+    <tr>
+      <th>WKT</th>
+      <th>Name</th>
+      <th>Area</th>
+      <th>Activity 1</th>
+      <th>Activity 2</th>
+      <th>Site Link</th>
+    </tr>
+    <?php
 
 
-  $TodaysDate = strtotime("now");
-  $WKT = NULL;
+    $TodaysDate = strtotime("now");
+    $WKT = NULL;
 
-  $sql = "SELECT * FROM `site` WHERE IsDeleted <> 1";
-  $site_sresults = $cGOAT->doQuery($sql);
+    $sql = "SELECT * FROM `site` WHERE IsDeleted <> 1";
+    $site_sresults = $cGOAT->doQuery($sql);
 
-  while ($site = $site_sresults->fetch_assoc()) {
-    $WKT = "POINT()";
-    $SiteLink = "Siteid=".$site['IDX'];
-    //$SiteURL = "<a href='https://goatbook.site/DisplayCampSite.php?".$SiteLink."'>https://goatbook.site/DisplayCampSite.php?".$SiteLink;
+    while ($site = $site_sresults->fetch_assoc()) {
+
+      $first_token  = strpos($site['embedmap'], "!2d");
+      $second_token = strpos($site['embedmap'], '!2m');
+      if(!$second_token)
+        $second_token = strpos($site['embedmap'], '!3m');
+      $Point = substr($site['embedmap'], $first_token + 3, $second_token- ($first_token+3));
+      $point = str_replace("!3d", " ", $Point);
+      $WKT = "POINT(".$point.")";
+      $SiteLink = "Siteid=" . $site['IDX'];
+      $SiteURL = "https://goatbook.site/DisplayCampSite.php?" . $SiteLink;
 
 
-        $WKT. '</td><td>' .
-        ucwords(strtolower($site['name'])). '</td><td>' .
-        $cGOAT->GetAreaText($site['area']). '</td><td>' .
-        $cGOAT->GetActivityText($site['type1']). '</td><td>' .
-        $cGOAT->GetActivityText($site['type2']). '</td><td>' .
-        $SiteLink. '<td></tr>';
+      echo "<tr><td>" .
+        $WKT . '</td><td>' .
+        ucwords(strtolower($site['name'])) . '</td><td>' .
+        $cGOAT->GetAreaText($site['area']) . '</td><td>' .
+        $cGOAT->GetActivityText($site['type1']) . '</td><td>' .
+        $cGOAT->GetActivityText($site['type2']) . '</td><td>' .
+        $SiteURL . '<td></tr>';
 
 
-      $csv_output .= "\n";
+      $csv_output .= $WKT. ",";
+      $csv_output .= ucwords(strtolower($site['name'])) . ",";
+      $csv_output .= $cGOAT->GetAreaText($site['area']) . ",";
+      $csv_output .= $cGOAT->GetActivityText($site['type1']) . ",";
+      $csv_output .= $cGOAT->GetActivityText($site['type2']) . ",";
+      $csv_output .= $SiteURL ."\n";
     }
 
-  echo "</table>";
-  
-  
-  ?>
-  <br /><br /><br />
-  <center>
-    <form name="export" action="export.php" method="post">
-      <input class='RoundButton' style="width:220px" type="submit" value="Export table to CSV">
-      <input type="hidden" value="<?php echo $csv_hdr; ?>" name="csv_hdr">
-      <input type="hidden" value="<?php echo $csv_output; ?>" name="csv_output">
-    </form>
-  </center>
-  <br />
+    echo "</table>";
+
+
+    ?>
+    <br /><br /><br />
+    <center>
+      <form name="export" action="export.php" method="post">
+        <input class='RoundButton' style="width:220px" type="submit" value="Export table to CSV">
+        <input type="hidden" value="<?php echo $csv_hdr; ?>" name="csv_hdr">
+        <input type="hidden" value="<?php echo $csv_output; ?>" name="csv_output">
+      </form>
+    </center>
+    <br />
 
 </body>
 
