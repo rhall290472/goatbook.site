cb0b5285ab01283ce330e621fc089aa920ad57ee
rhall290472 <56454660+rhall290472@users.noreply.github.com>
2025-09-06 08:27:04 -0600

    Added WYSIWYG editor to this page

--- EditCampSite.php
+++ EditCampSite.php
@@ -34,16 +34,32 @@ if (!session_id()) {
 
 <head>
   <?php include('head.php'); ?>
+  <!-- Include TinyMCE from CDN -->
+  <!-- <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script> -->
+  <script src="https://cdn.tiny.cloud/1/go7c0mdpiffej81ji1n8edfu4mubr4v4fnrz6dc5qzjhian8/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+
+  <script>
+    // Initialize TinyMCE on page load
+    document.addEventListener('DOMContentLoaded', function() {
+      tinymce.init({
+        selector: 'textarea#Notes',
+        plugins: 'lists link image table code',
+        toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        menubar: false,
+        height: 300,
+        content_style: 'body { font-family: Arial, sans-serif; font-size: 14px }'
+      });
+    });
+  </script>
 </head>
 
 <body>
   <?php
   include_once('header.php');
 
-
   //#####################################################
   //
-  // Check to see if user as Submitted the form. If so, save the data..
+  // Check to see if user has Submitted the form. If so, save the data.
   //
   //#####################################################
   if (isset($_POST['SubmitForm'])) {
@@ -59,46 +75,40 @@ if (!session_id()) {
     if ($ResultOld)
       $SiteOldData = $ResultOld->fetch_assoc();
 
-    // Save New data..From the user form
+    // Save New data from the user form
     $FormData = array();
     $FormData['IDX'] = $_POST['Siteid'];
-    $FormData['area'] =  $cGOAT->GetFormData('element_1_1');
-    $FormData['name'] =  $cGOAT->GetFormData('element_1_2') == "" ? 0 : ucwords(strtolower($cGOAT->GetFormData('element_1_2')));
-    $FormData['type1'] =  $cGOAT->GetFormData('element_1_3') == "" ? 0 : $cGOAT->GetFormData('element_1_3');
-    $FormData['type2'] =  $cGOAT->GetFormData('element_1_4') == "" ? 0 : $cGOAT->GetFormData('element_1_4');;
-    $FormData['map'] =  $cGOAT->GetFormData('element_2_1');
-    $FormData['facilities'] =  $cGOAT->GetFormData('element_2_2');
-    $FormData['IsDeleted'] =  $cGOAT->GetFormData('element_2_3');
-    $FormData['embedmap'] =  $cGOAT->GetFormData('element_3_1');
+    $FormData['area'] = $cGOAT->GetFormData('element_1_1');
+    $FormData['name'] = $cGOAT->GetFormData('element_1_2') == "" ? 0 : ucwords(strtolower($cGOAT->GetFormData('element_1_2')));
+    $FormData['type1'] = $cGOAT->GetFormData('element_1_3') == "" ? 0 : $cGOAT->GetFormData('element_1_3');
+    $FormData['type2'] = $cGOAT->GetFormData('element_1_4') == "" ? 0 : $cGOAT->GetFormData('element_1_4');
+    $FormData['map'] = $cGOAT->GetFormData('element_2_1');
+    $FormData['facilities'] = $cGOAT->GetFormData('element_2_2');
+    $FormData['IsDeleted'] = $cGOAT->GetFormData('element_2_3');
+    $FormData['embedmap'] = $cGOAT->GetFormData('element_3_1');
     $FormData['directions'] = addslashes($cGOAT->GetFormData('Notes'));
 
     if ($cGOAT->UpdateSite($FormData)) {
-      // Record has been updated in database now create a audit trail
+      // Record has been updated in database, now create an audit trail
       $cGOAT->CreateAudit($SiteOldData, $FormData);
-      //$cGOAT->GoToURL('./index.php');
       $cGOAT->GoToURL("./DisplayCampSite.php?Siteid=" . $FormData['IDX']);
     }
   }
 
-
-
-
   // Check which type of camp view they wish to view
   if (isset($_GET['Siteid'])) {
-    // create a table of all of the campsites selected
+    // Create a table of all of the campsites selected
     $site = $_GET['Siteid'];
     $sql = "SELECT * FROM `site` WHERE IDX = '" . $site . "'";
 
     $Result = $cGOAT->doQuery($sql);
     if (!$Result) {
       $cGOAT->function_alert("Error");
-      $cGOAT->GotoURL('./index.php');
+      $cGOAT->GoToURL('./index.php');
     } else {
       $Site = $Result->fetch_assoc();
     }
   }
-
-
 
   if (isset($Site)) {
   ?>
@@ -107,21 +117,16 @@ if (!session_id()) {
       if ((isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]) !== true) {
       ?>
         <center>
-          <p>You must have a account with the GOAT website and be logged on to edit a campsite.</p>
+          <p>You must have an account with the GOAT website and be logged on to edit a campsite.</p>
           <a class="btn btn-primary btn-sm" href="./logon.php">Log On</a>
         </center>
       <?php
       } else {
-        // Display a form to take the new camp site.
+        // Display a form to edit the camp site.
       ?>
         <div class="form-coach px-5" style="background-color: var(--scouting-lighttan);">
           <p style="text-align:Left"><b>Edit Camp Site Information</b></p>
           <form action="<?php echo $_SERVER['PHP_SELF']; ?>" id="coach-form" method="post">
-            <?php
-            //require_once('recaptchalib.php');
-            //$publickey = "your_public_key"; // you got this from the signup page
-            //echo recaptcha_get_html($publickey);
-            ?>
             <div class="form-row">
               <div class="col-3">
                 <label for=element_1_1>Area</label>
@@ -129,7 +134,7 @@ if (!session_id()) {
               </div>
               <div class="col-2">
                 <label for=element_1_2>Name</label>
-                <input type="text" name="element_1_2" class="form-control" <?php echo "value='" . ucwords($Site['name']) . "'";    ?> />
+                <input type="text" name="element_1_2" class="form-control" <?php echo "value='" . ucwords($Site['name']) . "'"; ?> />
               </div>
               <div class="col-3">
                 <label for=element_1_3>Primary Activity</label>
@@ -160,37 +165,34 @@ if (!session_id()) {
             <div class="form-row">
               <div class="col-12">
                 <label for=element_3_1>Google Embed Map</label>
-                <input type="text" name="element_3_1" class="form-control" <?php if (strlen($Site['embedmap']) > 0) echo "value='" . $Site['embedmap'] . "'";  ?> />
+                <input type="text" name="element_3_1" class="form-control" <?php if (strlen($Site['embedmap']) > 0) echo "value='" . $Site['embedmap'] . "'"; ?> />
               </div>
             </div>
             <div class="form-row">
               <div class="col-12">
                 <label for=Notes>Notes/How to get there</label>
-                <textarea class="form-control" name="Notes" rows="10" style="height:100%;"><?php if (strlen($Site['directions']) > 0) echo $Site['directions']; ?></textarea>
+                <textarea class="form-control" name="Notes" id="Notes" rows="10" style="height:100%;"><?php if (strlen($Site['directions']) > 0) echo $Site['directions']; ?></textarea>
               </div>
             </div>
             <div class="form-row">
               <div class="col-10 py-5">
                 <?php $ID = -1; ?>
-                <?php echo '<input type="hidden" name="Siteid" value="' . $Site['IDX'] . '"/>';
-                ?>
+                <?php echo '<input type="hidden" name="Siteid" value="' . $Site['IDX'] . '"/>'; ?>
                 <input id="saveForm3" class="btn btn-primary btn-sm" type="submit" name="SubmitForm" value="Save" />
                 <input id="saveForm4" class="btn btn-primary btn-sm" type="submit" name="SubmitForm" value="Cancel" />
               </div>
             </div>
           </form>
         </div>
-
-    <?php
+      <?php
       }
-    }
-    ?>
+      ?>
     </div>
+  <?php
+  }
+  ?>
 
-
-
-
-    <?php include('Footer.php'); ?>
+  <?php include('Footer.php'); ?>
 
 </body>
 
