21f0b965483a9de8a8d1f9eba64a0789a610199a
rhall290472 <56454660+rhall290472@users.noreply.github.com>
2025-09-06 08:42:16 -0600

    Center map on page

--- index.php
+++ index.php
@@ -31,12 +31,40 @@ if (!session_id()) {
 
 <head>
   <?php include('head.php'); ?>
+  <style>
+    /* Center the map iframe */
+    .map-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+      /* Optional: Add spacing above the map */
+    }
+
+    /* Ensure the iframe is responsive */
+    .map-container iframe {
+      max-width: 100%;
+      /* Prevent overflow on smaller screens */
+      width: 1080px;
+      /* Maintain original width */
+      height: 640px;
+      /* Maintain original height */
+    }
+
+    @media (max-width: 1200px) {
+      .map-container iframe {
+        width: 100%;
+        /* Adjust width to fit smaller screens */
+        height: 400px;
+        /* Reduce height for better mobile display */
+      }
+    }
+  </style>
 </head>
 
 <body>
   <?php
   include_once('header.php');
-
   ?>
   <div class="container-fluid">
     <div class="row flex-nowrap">
@@ -48,20 +76,21 @@ if (!session_id()) {
           This guide has been prepared for Scouts and Scouters in order to share campsites, hiking trails, and other activities that have been successfully tried by other units. This site is
           designed to be used by both new Scouters and the experienced Scouter; hopefully providing new experiences and locations to better enjoy the Colorado outdoors. The site has been designed
           to be printed on standard 8 1/2x11 paper, print outs of particular pages can be made and taken on your adventure. The GOAT Site has been grouped into chapters covering a geographic
-          region or area of the state such as the Guanella Pass Area. Chapters have one or more maps merged into the text.</p>
+          region or area of the state such as the Guanella Pass Area. Chapters have one or more maps merged into the text.
+        </p>
         <ul class="list-unstyled">
           <li>
-            <h5>Acknowledgement</h5>The Starting point of this web site is based on the 2000 GOAT Book which was create by then Denver Area Council, Order of the Arrow, Tahosa Loge which
+            <h5>Acknowledgement</h5>
+            The Starting point of this web site is based on the 2000 GOAT Book which was created by the then Denver Area Council, Order of the Arrow, Tahosa Lodge which
             promotes Scout camping using several different methods. One of these methods is through the G.O.A.T. Book, which provides Scouts, Scouters and campers in general a guide to campsites
             (and activities) in Colorado.
           </li>
           <li>
-          <iframe src="https://www.google.com/maps/d/embed?mid=1h1MwNhYsCUFLAFhf7EtC6E4GEa-lWtc&ehbc=2E312F&noprof=1" width="1080" height="640"></iframe>
-
+            <div class="map-container">
+              <iframe src="https://www.google.com/maps/d/embed?mid=1h1MwNhYsCUFLAFhf7EtC6E4GEa-lWtc&ehbc=2E312F&noprof=1" width="1080" height="640"></iframe>
+            </div>
           </li>
         </ul>
-
-
       </div>
     </div>
   </div>
